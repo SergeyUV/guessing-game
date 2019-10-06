a590d140d50dd986c0cb9526ef73d5fdcdb0835c
Sergey Vnukov <skivun@hotmail.com>
2019-10-06 23:27:05 +0300

    task: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
-    constructor() {}
+    
+    constructor() {
+
+        this.min=undefined;
+        this.max=undefined;
+        this.candidate=undefined;
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
+        
+        this.candidate = Math.round((this.max + this.min)/2);
 
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
